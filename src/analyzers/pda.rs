769490c1dcb5659ead8ededbd8f67004b736463d
Leonardo <leobasilio@gmail.com>
2019-05-28 14:17:28 -0300

    Reduções por estado

--- src/analyzers/pda.rs
+++ src/analyzers/pda.rs
@@ -1,18 +1,49 @@
 use std::collections::HashMap;
 
 #[derive(Clone,Copy)]
-pub enum ActionType {
+pub enum ActionMethod {
     ACCEPT,
     SHIFT,
     REDUCE
 }
 
+#[derive(Clone,Copy)]
+struct Action {
+    method: ActionMethod,
+    param: i8
+}
+
+#[derive(Clone)]
+struct Rule {
+    left_side: String,
+    right_side: String
+}
+
+#[derive(Clone)]
+struct Reduction {
+    rule_nr: i8,
+    pop_count: i8
+}
+
 pub struct PDA {
+
     stack: Vec<i8>,
-    actions: HashMap<(i8,String),(ActionType,i8)>,
-    gotos: HashMap<(i8,String),i8>,
-    reductions: HashMap<i8,(String, String,i8)>,
-    follows: HashMap<i8,Vec<String>>
+
+    // (state,terminal) -> action
+    actions: HashMap<(i8, String), Action>,
+
+    // (state,non_terminal) -> new_state
+    gotos: HashMap<(i8, String), i8>,
+
+    // rule_nr -> rule
+    rules: HashMap<i8, Rule>,
+
+    // non_terminal -> follow_set
+    follows: HashMap<String, Vec<String>>,
+
+    // state -> State
+    reductions: HashMap<i8, Reduction>
+
 }
 
 impl PDA {
@@ -23,15 +54,19 @@ impl PDA {
             stack: vec![0],
             actions: HashMap::new(),
             gotos: HashMap::new(),
-            reductions: HashMap::new(),
-            follows: HashMap::new()
+            rules: HashMap::new(),
+            follows: HashMap::new(),
+            reductions: HashMap::new()
         }
 
     }
 
-    pub fn add_action(&mut self, state: i8, token: &str, action_type: ActionType, action_param: i8){
+    pub fn add_action(&mut self, state: i8, token: &str, action_method: ActionMethod, action_param: i8){
 
-        self.actions.insert((state, String::from(token)), (action_type, action_param));
+        self.actions.insert((state, String::from(token)), Action {
+            method: action_method,
+            param: action_param
+        });
 
     }
 
@@ -41,17 +76,27 @@ impl PDA {
 
     }
 
-    pub fn add_reduction(&mut self, rule_nr: i8, left_side: &str, right_side: &str){
+    pub fn add_rule(&mut self, rule_nr: i8, left_side: &str, right_side: &str){
 
-        self.reductions.insert(rule_nr, (String::from(left_side),
-                                         String::from(right_side),
-                                         right_side.split(' ').count() as i8));
+        self.rules.insert(rule_nr, Rule {
+            left_side: left_side.to_string(),
+            right_side: right_side.to_string()
+        });
 
     }
 
-    pub fn add_follow(&mut self, state: i8, follow_set: &[&str]){
+    pub fn add_follow(&mut self, non_terminal: &str, follow_set: &[&str]){
 
-        self.follows.insert(state, follow_set.iter().map(|s| s.to_string()).collect());
+        self.follows.insert(non_terminal.to_string(), follow_set.iter().map(|s| s.to_string()).collect());
+
+    }
+
+    pub fn add_reduction(&mut self, state: i8, rule_nr: i8, pop_count: i8){
+
+        self.reductions.insert(state, Reduction {
+            rule_nr,
+            pop_count
+        });
 
     }
 
@@ -61,22 +106,21 @@ impl PDA {
 
             if let Some(&current_state) = self.stack.last() {
 
-                if let Some(&(action_type, action_param)) =
-                   self.actions.get(&(current_state, lexeme.clone())) {
+                if let Some(&action) = self.actions.get(&(current_state, lexeme.clone())) {
 
-                    match action_type {
+                    match action.method {
 
-                        ActionType::SHIFT => {
+                        ActionMethod::SHIFT => {
 
-                            self.shift(action_param);
+                            self.shift(action.param);
 
                             return Ok(false);
 
                         },
 
-                        ActionType::REDUCE => {
+                        ActionMethod::REDUCE => {
 
-                            if let Err(e) = self.reduce(action_param) {
+                            if let Err(e) = self.reduce(current_state) {
 
                                 return Err(e);
 
@@ -84,7 +128,7 @@ impl PDA {
 
                         },
 
-                        ActionType::ACCEPT => return Ok(true)
+                        ActionMethod::ACCEPT => return Ok(true)
 
                     }
 
@@ -106,39 +150,47 @@ impl PDA {
 
     }
 
-    fn reduce(&mut self, rule_nr: i8) -> Result<(), String> {
+    fn reduce(&mut self, current_state: i8) -> Result<(), String> {
 
-        if let Some((rule_left_side, _rule_right_side, rule_pop_count)) = self.reductions.get(&rule_nr) {
+        if let Some(reduction) = self.reductions.get(&current_state) {
 
-            for _ in 0..*rule_pop_count {
+            if let Some(rule) = self.rules.get(&reduction.rule_nr) {
 
-                self.stack.pop();
+                for _ in 0..reduction.pop_count {
 
-            }
+                    self.stack.pop();
 
-            if let Some(&current_state) = self.stack.last() {
+                }
 
-                if let Some(&new_state) = self.gotos.get(&(current_state, rule_left_side.to_string())) {
+                if let Some(&current_state) = self.stack.last() {
 
-                    self.stack.push(new_state);
+                    if let Some(&new_state) = self.gotos.get(&(current_state, rule.left_side.to_string())) {
 
-                    return Ok(())
+                        self.stack.push(new_state);
+
+                        return Ok(())
+
+                    }else{
+
+                        return Err(format!("Desvio não encontrado [{},{}]", current_state, rule.left_side));
+
+                    }
 
                 }else{
 
-                    return Err(format!("Desvio não encontrado [{},{}]", current_state, rule_left_side));
+                    return Err(format!("Pilha vazia na redução [{},{}]", reduction.pop_count, rule.left_side));
 
                 }
 
             }else{
 
-                return Err(format!("Pilha vazia na redução [{},{}]", rule_pop_count, rule_left_side));
+                return Err(format!("Regra não encontrada [{}]", reduction.rule_nr));
 
             }
 
         }else{
 
-            return Err(format!("Redução não encontrada [{}]", rule_nr));
+            return Err(format!("Redução não encontrada [{}]", current_state));
 
         }
 
@@ -146,9 +198,12 @@ impl PDA {
 
     fn error(&mut self, current_state: i8, lexeme: &String){
 
-        let transitions_count = self.actions.keys().filter(|k| k.0 == current_state).count();
+        let actions : Vec<&String> = self.actions.keys()
+                                                 .filter(|(state, _)| *state == current_state)
+                                                 .map(|(_, terminal)| terminal)
+                                                 .collect();
 
-        if transitions_count == 1 {
+        if actions.len() == 1 {
 
 
 
